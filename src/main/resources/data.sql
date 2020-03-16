a71d36e0e15f0fe68ffb615d2ba6665466da76cd
Anderson Guilherme da Silva Almeida <anderson.almeida@pampa.compasso>
2020-03-16 16:56:14 -0300

    Autenticando o usuário.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$MGJS0nefHHGrGa4uBT7lruZR9u/nhZ5XBSXAxpUsz5Cz9GZNIM30m');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
